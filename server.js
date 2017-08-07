835a536a3bf8942ed42d48af9dc99c1c3c1fbb0e
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-07 15:37:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ function createTemp(data) {
                     ${title}
                 </title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
             </head>
             <body>
                 <div>
