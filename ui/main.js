f6926944941c4a3f403be610f8015e9547636fb6
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-08 22:31:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'JS: I m alive !!!';
 
 var img = document.getElementById('vicky');
+var marginLeft =0;
 
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = 'px';
+}
 img.onclick = function() {
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
