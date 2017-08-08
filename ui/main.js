da1e90fe716775febde99430367944cd1238c55f
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-08 22:20:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'JS: I m alive !!!';
 
-var img = document.getElementById('img');
+var img = document.getElementById('vicky');
 
 img.onclick = function() {
     img.style.marginleft = '100px';
