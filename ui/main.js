dbd09bd6d38d6db14fc3119e4eae5b4d42c0838b
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-16 22:16:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,16 +25,27 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn')
 
 submit.onclick = function() {
-    // 
+    // Make req
+    var req = new XMLHttpRequest();
     
-    //Capture list 
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
+    // Capture the resp and stroe it in  variable 
+    req.onreadystatechange = function () {
+        if (req.readyState === XMLHttpRequest.DONE) {
+            if (req.status === 200) {
+                //Capture list 
+                var names = req.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    req.open('GET', 'http://ecomvicky.imad.hasura-app.io/submit-name?name=' + name, true);
+    req.send(null);
     
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
     
 };
