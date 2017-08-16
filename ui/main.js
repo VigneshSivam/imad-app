863272dff1416c66d7add39c96176216aafed331
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-16 21:55:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,22 @@ button.onclick = function() {
     req.open('GET', 'http://ecomvicky.imad.hasura-app.io/counter', true);
     req.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn')
+
+submit.onclick = function() {
+    // 
+    
+    //Capture list 
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
