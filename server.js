037ce8e62b17696f290e4df446f420eefca640e7
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-19 15:24:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'ecomvicky',
+    database: 'ecomvicky',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 
 var app = express();
