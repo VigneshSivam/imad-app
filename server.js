7ffb7bc6981dcecdb8aad3ad4f30d3fce533d01b
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-23 19:29:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'ecomvicky',
@@ -19,6 +20,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input, salt) {
+    // How to creat a hash 
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+    
+}
+app.get('/hash/:input',  function(req, res){
+   var hashedString = hash(req.params.input, 'random-string');
+   res.send(hashedString);
+});
 var pool = new Pool(config);
 app.get('/test-db', function(err,res) {
    pool.query('SELECT * FROM test', function(err,result) {
