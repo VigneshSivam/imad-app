ff2037d61dcd5767b388b27384c507d85b92db2e
VigneshSivam <30698103+VigneshSivam@users.noreply.github.com>
2017-08-24 21:47:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,3 @@
-// Counter on click code 
-var button = document.getElementById('counter');
-
-button.onclick = function() {
-    // Make request to the counter endpoint
-    var req = new XMLHttpRequest();
-    
-    // Capture the resp and stroe it in  variable 
-    req.onreadystatechange = function () {
-        if (req.readyState === XMLHttpRequest.DONE) {
-            if (req.status === 200) {
-                var counter = req.responseText;
-                var span    = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    req.open('GET', 'http://ecomvicky.imad.hasura-app.io/counter', true);
-    req.send(null);
-};
-
 
 var submit = document.getElementById('submit_btn')
 
